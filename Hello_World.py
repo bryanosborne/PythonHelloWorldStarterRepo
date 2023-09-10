96a095e48d33fb2f320ac36fdfe13376bbebfca1
J. Bryan Osborne <jbryanosborne@gmail.com>
2023-09-10 12:33:51 -0500

    Create Hello_World.py

---
+++ Hello_World.py
@@ -0,0 +1,18 @@
+# Program: helloworld.py
+# Author: 
+# Create date:  
+# Description: 
+#  
+# 
+
+# Make any needed imports
+
+# Define variables
+
+############### Main ###############
+
+# Print the greeting
+
+
+
+
